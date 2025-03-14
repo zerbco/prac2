5987eda928a8e0effd72beb51079d6808723baf4
zerbco <federicoektn09@gmail.com>
2025-03-14 19:31:28 +1030

    Add files via upload

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -13,9 +13,14 @@ void print_binary_str(string decimal_number) {
     string binary = "";
 
     while (num > 0) {
-        binary = binary + to_string(num % 2);
+        binary = to_string(num % 2) + binary;  
         num /= 2;
     }
-
+    
+    string reversed = "";
+    for (int i = binary.length() - 1; i >= 0; i--) {
+        reversed += binary[i];
+    }
+    
     cout << binary << endl;
 }
